c30b7145200a1c8d51fe9a9f43ce0fdffbe58b0f
taha taheri <144992748+tanhadel@users.noreply.github.com>
2025-12-05 08:59:13 +0100

    Fix Railway NuGet cache path issue

--- obj/Debug/net9.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03cdf06130c476a35cf549d5bd8d3349021ee38a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2330b30ae639fa87367936c63608386a8fefe190")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
