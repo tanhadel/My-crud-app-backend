f54f72ae07b7dd1d2e17e333b4f01510d20ed548
taha taheri <144992748+tanhadel@users.noreply.github.com>
2025-12-05 09:25:50 +0100

    Remove .sln file and add .gitignore

--- obj/Debug/net9.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2330b30ae639fa87367936c63608386a8fefe190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9f94ebbaeb4ec62233fd3bd5463be2d06027680")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
